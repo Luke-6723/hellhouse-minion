7c1a8c2ef5a21fbfb9deabdb2d7945d9c795bdc6
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 16:04:05 +0100

    commenting and code refactor

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,10 +1,12 @@
 const { connect } = require('amqplib/callback_api')
 const { EventEmitter } = require('events')
+const Logger = require('../Logger')
 
 class Client extends EventEmitter {
   constructor (config = { amqpUser: '', amqpPass: '', amqpHost: '', amqpPrefix: '' }) {
     super()
     this.config = config
+    this.Logger = new Logger('CLIENT')
     this.queueName = `${config.amqpPrefix}:SEND`
     this.events = {
       MESSAGE_CREATE: 'message'
@@ -21,10 +23,10 @@ class Client extends EventEmitter {
         this.emit('ready')
         // Start consuming incoming events
         ch.consume(this.queueName, (message) => {
-          this.emit(this.events[message.fields.routingKey] || 'debug', {
-            client: this,
-            ...JSON.parse(message.content)
-          })
+          // Parse buffer
+          const parsedMessage = JSON.parse(message.content)
+          // Emit the event
+          this.emit(this.events[message.fields.routingKey] || 'debug', { client: this, ...parsedMessage })
         }, { noAck: true })
       })
     })
