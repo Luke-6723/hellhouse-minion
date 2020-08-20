280df467577ffee1f4b37b96f41203f555df13f2
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 19:52:45 +0100

    handle caching

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,15 @@
 const Channel = require('../Discord/Channel')
+const redis = require('../Redis')
+const config = require('../config.json')
+const Util = require('../Util')
 
 module.exports = async (msg) => {
+  let channel = await redis.get(`${config.redis}:channels:${msg.channel_id}`) || new Channel(msg.client, { id: msg.channel_id, guildID: msg.guild_id })
+  if (typeof channel === 'string') {
+    const instance = new Channel(msg.client, { fromCache: true })
+    channel = await Util.deserializeClass(instance, channel)
+  }
+  console.log(channel)
   if (msg.author.bot) return
   msg.client.Logger.send('MESSAGE', `Recieved message from ${msg.author.id}`)
 }
