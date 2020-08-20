858d3d9a9cff9b70b28e60bcda2602fbaf0b7b03
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 19:52:53 +0100

    serialization helpers

--- src/Util/index.js
+++ src/Util/index.js
@@ -1,3 +1,14 @@
 exports.channelTypes = {
   0: 'TEXT'
 }
+
+exports.serializeClass = async function (instance) {
+  return JSON.stringify(instance)
+}
+
+exports.deserializeClass = async function (instance, obj) {
+  var serializedObject = await JSON.parse(obj)
+  await Object.assign(instance, serializedObject)
+  instance.cached = true
+  return instance
+}
