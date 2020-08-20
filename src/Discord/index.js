527a543fae86fadd6922cd7a2e7ae656ff59e1d9
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 14:52:13 +0100

    fix

--- src/Discord/index.js
+++ src/Discord/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  Channel: = require('./Channel'),
-  Guild: = require('./Guild'),
-  User: = require('./User'),
-  UserFlags: = require('./UserFlags')
+  Channel: require('./Channel'),
+  Guild: require('./Guild'),
+  User: require('./User'),
+  UserFlags: require('./UserFlags')
 }
