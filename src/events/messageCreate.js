790276be7e810b7bc3dd23ca0c24cce08d45723e
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 16:09:49 +0100

    message create event

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,3 +1,6 @@
+const Channel = require('../Discord/Channel')
+
 module.exports = async (msg) => {
-  console.log(msg)
+  if (msg.author.bot) return
+  msg.client.Logger.send('MESSAGE', `Recieved message from ${msg.author.id}`)
 }
