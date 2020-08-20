793c7d106fb8a9befd2fa050a4db48fd5650a824
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 16:04:23 +0100

    add error to logging handler

--- src/Logger/index.js
+++ src/Logger/index.js
@@ -5,6 +5,10 @@ class Logger {
     this.name = name
   }
 
+  error (event, message, ...args) {
+    console.log(`[${chalk.red.dim('ERROR')}] [${chalk.red.bold(event)}] ${message} ${args.join(' ')}`)
+  }
+
   send (event, message, ...args) {
     console.log(`[${chalk.yellow.dim(this.name)}] [${chalk.green.bold(event)}] ${message} ${args.join(' ')}`)
   }
