8b461716bf6fb3674b9f117398b1ac9bb40cede9
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-20 16:04:12 +0100

    add message sending

--- src/Discord/Channel.js
+++ src/Discord/Channel.js
@@ -1,9 +1,38 @@
+const fetch = require('node-fetch')
+const Util = require('../Util')
+const { discord, bot } = require('../config.json')
+
 class Channel {
-  constructor (id, name, channelType) {
-    this.id = id
-    this.name = name
-    this.guild = {}
-    this.channelType = channelType
+  constructor (client, options = { id: '', name: '', channelType: 0, guildID: '' }) {
+    this.client = client
+    this.id = options.id
+    this.name = options.name
+    this.guild = options.guildID
+    this.channelType = Util.channelTypes[this.channelType]
+  }
+
+  async send (msg) {
+    let body
+    if (typeof msg === 'string') body = { content: msg }
+    else body = msg
+
+    body = JSON.stringify(body)
+
+    await fetch(`${discord.discordApiUrl}/${discord.apiVersion}/channels/${this.id}/messages`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bot ${bot.token}`
+      },
+      body: body
+    }).then(async messagePost => {
+      if (messagePost.status === 429) {
+        const response = await messagePost.json()
+        const sendFunc = this.send.bind(this)
+        this.client.Logger.error('RATELIMIT', 'We are being ratelimited. Retrying after', response.retry_after, 'ms')
+        setTimeout(async () => { sendFunc(msg) }, response.retry_after)
+      }
+    })
   }
 }
 
