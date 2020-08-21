85c01a2b9cd67b7ea106f18fd7a1658dbbc6d71a
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 21:15:42 +0100

    message create logging

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,4 +1,8 @@
+const Util = require('../Util')
+const botConfig = require('../config.json').bot
+
 module.exports = async (client, msg) => {
-  if (!msg.author.bot) return
-  console.log('Hello')
+  if (msg.author.bot) return
+  const user = await Util.redis.get(`hellhouse:user:${msg.author.id}`) || await Util.mongo.users.findOne({ _id: msg.author.id }).catch(() => {}) || { prefix: botConfig.prefix }
+  console.log(user)
 }
