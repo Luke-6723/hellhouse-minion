877db584aa38ac351b8a4b171df4cd89da17022d
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 20:02:54 +0100

    remove newline

--- src/Events/index.js
+++ src/Events/index.js
@@ -7,9 +7,8 @@ class Events {
   constructor (client) {
     this.logEvent = new Logger('EVENT')
     this.client = client
-
     this.client.on('ready', () => this.logEvent.send('READY', 'Logged in as', this.client.user.tag))
-    this.client.on('message', (msg) => { eventHandlers.messageCreate(client, msg) })
+    this.client.on('message', (msg) => { eventHandlers.messageCreate(this.client, msg) })
   }
 }
 
