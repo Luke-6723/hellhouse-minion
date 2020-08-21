57c75a19be2cf6c1a0934765cb62b8622bf17389
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 20:02:34 +0100

    add mongo to util

--- src/Util/index.js
+++ src/Util/index.js
@@ -1,3 +1,28 @@
+const Redis = require('redis')
+const { promisify } = require('util')
+const { redisHost, redisPass } = require('../config.json').redis
+const { database } = require('../config.json')
+const mongoose = require('mongoose')
+const Logger = require('../Logger')
+const log = new Logger('UTILS')
+
+// DB 0 - Session handling, DB 1 - Other stuff DB 2 Bot
+const redisClient = Redis.createClient({ host: redisHost, port: '6379', password: redisPass, db: 1 })
+const commands = ['get', 'set', 'del', 'ttl']
+commands.forEach(command => { redisClient[command] = promisify(redisClient[command]).bind(redisClient) })
+redisClient.on('ready', () => { log.send('REDIS', 'Connected') })
+
+const mongo = mongoose.connect(`mongodb://${database.mongoIp}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  auth: {
+    user: database.mongoUser,
+    password: database.mongoPass
+  },
+  authSource: 'admin',
+  dbName: database.mongoDbName
+}).then(() => { log.send('MONGO', 'connected') })
+
 exports.channelTypes = {
   0: 'TEXT'
 }
@@ -12,3 +37,6 @@ exports.deserializeClass = async function (instance, obj) {
   instance.cached = true
   return instance
 }
+
+exports.redis = redisClient
+exports.mongo = mongo
