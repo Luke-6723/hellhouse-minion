6d1932f44a0fae2c2c2682fe69716c86161ee963
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-22 00:44:25 +0100

    add id to scema

--- src/Mongo/index.js
+++ src/Mongo/index.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose')
 const { bot } = require('../config.json')
 
 const UserSchema = new Schema({
+  id: { type: String, required: true },
   prefix: { type: String, required: true, default: bot.prefix },
   warnings: Array,
   leveling: Object
