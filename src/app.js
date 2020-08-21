79b1e004137eb165aa444fba04e737e08213cb22
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 23:49:46 +0100

    code refactor

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const discord = require('discord.js')
 const botConfig = require('./config.json').bot
 const Events = require('./Events')
 const Logger = require('./Logger')
-const CommandHandler = require('../CommandHandler')
+const CommandHandler = require('./CommandHandler')
 
 class HellhouseMinion {
   constructor () {
-    this.client = new discord.Client()
     this.logger = new Logger('CLIENT')
+    this.client = new discord.Client()
+    this.client.commands = new CommandHandler()
     this.events = new Events(this.client)
   }
 
   async login () {
     await this.client.login(botConfig.token)
-    this.client.commands = new CommandHandler()
   }
 }
 
