c9f12ab00ea539d4d3cd4ed8c7d72f7314de31d1
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 23:37:44 +0100

    remove command handler from message event

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,7 +1,5 @@
 const Util = require('../Util')
 const botConfig = require('../config.json').bot
-const CommandHandler = require('../CommandHandler')
-const commands = new CommandHandler()
 
 module.exports = async (client, msg) => {
   if (msg.author.bot) return
@@ -10,7 +8,7 @@ module.exports = async (client, msg) => {
   const givenPrefix = content.subtr(0, user.prefix.length)
   const givenCommand = content.substr(user.prefix.length).split(' ')[0]
   const commandArgs = content.substr(user.prefix.length).split(' ').splice(1)
-  if (givenPrefix === user.prefix && commands[givenCommand]) {
-    await commands[givenCommand](client, msg, commandArgs)
+  if (givenPrefix === user.prefix && client.commands[givenCommand]) {
+    await client.commands[givenCommand](client, msg, commandArgs)
   }
 }
