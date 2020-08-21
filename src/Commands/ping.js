40e34771aad2fa157e63bfed868b01a6ee8f8d78
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-22 00:10:24 +0100

    better ping command

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -1,3 +1,26 @@
 module.exports = async (client, msg, args) => {
-  await msg.channel.send('Pong')
+  const timeOne = new Date()
+  const message = await msg.channel.send({
+    embed: {
+      color: 0xF44444,
+      title: 'Discord Ping:',
+      description: `
+Message Ping: \`...\`
+
+Websocket Ping: \`...\`
+      `
+    }
+  })
+  const timeTwo = new Date()
+  message.edit({
+    embed: {
+      color: 0xF44444,
+      title: 'Discord Ping:',
+      description: `
+Message Ping: \`${timeTwo - timeOne}ms\`
+
+Websocket Ping: \`${client.ws.ping}ms\`
+      `
+    }
+  })
 }
