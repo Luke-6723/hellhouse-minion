694e74acbb556c72ca65abba0a09c5c2171a0e86
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-21 23:52:38 +0100

    i mispelt a fuction

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -5,7 +5,7 @@ module.exports = async (client, msg) => {
   if (msg.author.bot) return
   const user = await Util.mongo.users.findOne({ _id: msg.author.id }).catch(() => {}) || { prefix: botConfig.prefix }
   const content = msg.content.replace(/[ ]+/g, ' ')
-  const givenPrefix = content.subtr(0, user.prefix.length)
+  const givenPrefix = content.substr(0, user.prefix.length)
   const givenCommand = content.substr(user.prefix.length).split(' ')[0]
   const commandArgs = content.substr(user.prefix.length).split(' ').splice(1)
   if (givenPrefix === user.prefix && client.commands[givenCommand]) {
