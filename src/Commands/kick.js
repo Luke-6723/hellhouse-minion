0ea6a4b57461953758813895f48b02bdd6576b4f
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-01 06:48:50 +0100

    kick command

---
+++ src/Commands/kick.js
@@ -0,0 +1,34 @@
+const { defaultEmbedColor } = require('../Util')
+
+module.exports = async (client, msg, args) => {
+  if (!msg.member.hasPermission('KICK_MEMBERS')) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        title: 'You don\'t have permission to perform this acction.',
+        description: 'You require `KICK_MEMBERS` to run this command.'
+      }
+    })
+  }
+  if (!args[0]) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'Please give a user to kick'
+      }
+    })
+  }
+  const userid = args[0].replace(/[<@!>]/g, '')
+  const member = msg.guild.members.cache.get(userid)
+  if (!member) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'That user is not in the server.'
+      }
+    })
+  }
+  const reason = args.splice(1).join(' ') || undefined
+  await member.kick(`[${msg.author.tag}] ${reason}`)
+  return msg.channel.send({ embed: { color: defaultEmbedColor, description: `👢 **Kicked** ${member.user.tag} (<@${member.id}>)` } })
+}
