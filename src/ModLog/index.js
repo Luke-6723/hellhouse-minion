cc8e5c720bab8856d5d80e8196172f93d06e153d
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-04 06:46:43 +0100

    start modlog

--- src/ModLog/index.js
+++ src/ModLog/index.js
@@ -1,5 +1,20 @@
 const { ModLogs } = require('../Mongo').models
+const { modLogChannel } = require('../config.json').bot
 
-exports.addBan = async (msg_id, user_id, moderator_id, reason) {
+const sendEmbed = async (client, caseid, action, target = { mention: '', id: '' }, moderator = { mention: '', id: '' }, reason) => {
+  client.channels.cache.get(modLogChannel)
+}
 
+exports.addBan = async (client, msg, user, moderator, reason) => {
+  const caseId = (await ModLogs.find({})).length + 1
+  const modlogMessage = await sendEmbed(client, caseId, 'BAN', user, moderator, reason)
+  const modlogCase = new ModLogs({
+    case: caseId,
+    action: 'BAN',
+    moderator_id: moderator.id,
+    user_id: user.id,
+    reason: reason,
+    message_id: modlogMessage.id
+  })
+  await modlogCase.save()
 }
