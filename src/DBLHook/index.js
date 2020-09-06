cfc66ad440c2a0f4ec04a618dc3528444cb9ab04
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-06 03:48:55 +0100

    update

--- src/DBLHook/index.js
+++ src/DBLHook/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/webhook', async (req, res) => {
-  const data = await req.json()
+  const data = req.body
   console.log(data)
 })
 
