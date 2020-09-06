313460abcc227e9a25a536ec396ce985ba5c54b4
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-06 04:48:20 +0100

    add dsl hook event

--- src/Events/index.js
+++ src/Events/index.js
@@ -1,12 +1,13 @@
 const Logger = require('../Logger')
 const eventHandlers = {
-  messageCreate: require('./messageCreate')
+  messageCreate: require('./messageCreate'),
+  userVote: require('./userVote')
 }
 
 class Events {
   constructor (client) {
-    this.logEvent = new Logger('EVENT')
-    this.client = client
+    this.logEvent = new Logger(' EVENT ')
+    this.client = client.DSLHook.on('vote', eventHandlers.userVote)
     this.client.on('ready', () => this.logEvent.send('READY', 'Logged in as', this.client.user.tag))
     this.client.on('message', (msg) => { eventHandlers.messageCreate(this.client, msg) })
   }
