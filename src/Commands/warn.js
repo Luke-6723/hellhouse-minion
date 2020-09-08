a051f2581bdec1a7e6a74a9bf1ddd57139e79a84
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-08 03:09:04 +0100

    warn command

---
+++ src/Commands/warn.js
@@ -0,0 +1,45 @@
+const { defaultEmbedColor } = require('../Util')
+const ModLog = require('../ModLog')
+
+module.exports = async (client, msg, args) => {
+  if (!msg.member.roles.cache.map(r => r.name).includes('Moderator')) return
+  if (!msg.member.hasPermission('MANAGE_MEMBERS')) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        title: 'You don\'t have permission to perform this acction.',
+        description: 'You require `MANAGE_MEMBERS` to run this command.'
+      }
+    })
+  }
+  if (!args[0]) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'Please give a user to warn'
+      }
+    })
+  }
+  const userid = args[0].replace(/[<@!>]/g, '')
+  const member = msg.guild.members.cache.get(userid)
+  if (!member) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'That user is not in the server.'
+      }
+    })
+  }
+  if (args.splice(1).length === 0) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'Please give a warning to give.'
+      }
+    })
+  }
+  const reason = args.splice(1).join(' ') || undefined
+  await member.kick(`[${msg.author.tag}] ${reason}`)
+  await ModLog.addKick(client, member, msg.author, reason)
+  return msg.channel.send({ embed: { color: defaultEmbedColor, description: `🔈 **Warned** ${member.user.tag} (<@${member.id}>) for:\n\n${reason}` } })
+}
