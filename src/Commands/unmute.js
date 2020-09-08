4746a694a0f31070790edbdcdd56ec228d3b379e
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-08 06:26:14 +0100

    unmute command

---
+++ src/Commands/unmute.js
@@ -0,0 +1,46 @@
+const { defaultEmbedColor } = require('../Util')
+const { Mutes } = require('../Mongo').models
+const ModLog = require('../ModLog')
+
+module.exports = async (client, msg, args) => {
+  if (!msg.member.roles.cache.map(r => r.name).includes('Moderator')) return
+  if (!msg.member.hasPermission('MANAGE_ROLES')) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        title: 'You don\'t have permission to perform this acction.',
+        description: 'You require `MANAGE_ROLES` to run this command.'
+      }
+    })
+  }
+  if (!args[0]) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'Please give a user to unmute'
+      }
+    })
+  }
+  const userid = args[0].replace(/[<@!>]/g, '')
+  const member = msg.guild.members.cache.get(userid)
+  if (!member) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'That user is not in the server.'
+      }
+    })
+  } else if (!member.roles.cache.find(r => r.name.toLowerCase() === 'muted')) {
+    return msg.channel.send({
+      embed: {
+        color: defaultEmbedColor,
+        description: 'That user isn\'t already muted.'
+      }
+    })
+  }
+  const reason = args.splice(1).join(' ') || undefined
+  const role = msg.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted')
+  await member.roles.remove(role, `[${msg.author.tag}] ${reason}`)
+  await ModLog.addUnmute(client, member.user, msg.author, reason)
+  return msg.channel.send({ embed: { color: defaultEmbedColor, description: `😄 **Unmuted** ${member.user.tag} (<@${member.id}>)` } })
+}
