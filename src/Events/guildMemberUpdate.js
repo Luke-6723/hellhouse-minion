5381c6476ac1487c9a7f9450c95eefec09213e82
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-28 21:51:15 +0100

    guild member mute handling

---
+++ src/Events/guildMemberUpdate.js
@@ -0,0 +1,29 @@
+const ModLog = require('../ModLog')
+
+module.exports = async (oldMember, newMember) => {
+  console.log(1)
+  const oldMemberRoles = oldMember.roles.cache.map(r => r.name.toLowerCase())
+  const newMemberRoles = newMember.roles.cache.map(r => r.name.toLowerCase())
+  const guild = oldMember.guild
+  if (oldMemberRoles.includes('muted') && !newMemberRoles.includes('muted')) {
+    setTimeout(async () => {
+      const auditLog = (await guild.fetchAuditLogs({
+        type: 'MEMBER_ROLE_UPDATE',
+        limit: 1
+      })).entries.first()
+      if (!auditLog) return
+      if (auditLog.executor.id === guild.client.user.id) return
+      ModLog.addUnmute(guild.client, auditLog.target, auditLog.executor, ModLog.reason)
+    }, 250)
+  } else if (!oldMemberRoles.includes('muted') && newMemberRoles.includes('muted')) {
+    setTimeout(async () => {
+      const auditLog = (await guild.fetchAuditLogs({
+        type: 'MEMBER_ROLE_UPDATE',
+        limit: 1
+      })).entries.first()
+      if (!auditLog) return
+      if (auditLog.executor.id === guild.client.user.id) return
+      ModLog.addMute(guild.client, auditLog.target, auditLog.executor, ModLog.reason)
+    }, 250)
+  }
+}
