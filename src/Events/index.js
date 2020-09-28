4121daf389a9ee9b4b95d04da10ba9b80372addb
Luke-6723 <lukemorris2015@hotmail.com>
2020-09-28 21:51:21 +0100

    event handing

--- src/Events/index.js
+++ src/Events/index.js
@@ -5,7 +5,10 @@ const config = require('../config.json').bot
 const ModLog = require('../ModLog')
 const eventHandlers = {
   messageCreate: require('./messageCreate'),
-  userVote: require('./userVote')
+  userVote: require('./userVote'),
+  guildMemberBan: require('./guildMemberBan'),
+  guildMemberUnban: require('./guildMemberUnban'),
+  guildMemberUpdate: require('./guildMemberUpdate')
 }
 
 class Events {
@@ -15,6 +18,9 @@ class Events {
     this.client.DSLHook.on('vote', eventHandlers.userVote)
     this.client.on('ready', () => this.logEvent.send('DISCRDWS', 'Logged in as', this.client.user.tag))
     this.client.on('message', (msg) => { eventHandlers.messageCreate(this.client, msg) })
+    this.client.on('guildBanAdd', eventHandlers.guildMemberBan)
+    this.client.on('guildBanRemove', eventHandlers.guildMemberUnban)
+    this.client.on('guildMemberUpdate', eventHandlers.guildMemberUpdate)
 
     this.client.runUnmuteCheck = async () => {
       const allMutes = await Mutes.find({})
